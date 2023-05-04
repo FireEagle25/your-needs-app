d039688bf880c4f92114248a1287044b10f29e07
Nechaev Pavel <nechaev.p.v.25@gmail.com>
2023-05-04 17:11:45 +0700

    tests: добавлены тесты для книг и авторов

---
+++ tests/Feature/Api/AuthorControllerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Author;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use function PHPUnit\Framework\assertEmpty;
+
+class AuthorControllerTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    public function testIndex()
+    {
+        $response = $this->getJson(route('api.authors.index'));
+
+        $response->assertOk();
+    }
+
+    public function testStore()
+    {
+        $response = $this->postJson(route('api.authors.store'), [
+            'name' => $this->faker->name,
+        ]);
+
+        $response->assertCreated();
+        $this->assertDatabaseHas('authors', [
+            'id' => $response->json('data.id'),
+            'name' => $response->json('data.name'),
+        ]);
+    }
+
+    public function testShow()
+    {
+        $author = Author::factory()->create();
+
+        $response = $this->getJson(route('api.authors.show', $author->id));
+
+        $response->assertOk();
+    }
+
+    public function testUpdate()
+    {
+        $author = Author::factory()->create();
+
+        $response = $this->putJson(route('api.authors.update', $author->id), [
+            'name' => $this->faker->name,
+        ]);
+
+        $response->assertOk();
+        $this->assertDatabaseHas('authors', [
+            'id' => $author->id,
+            'name' => $response->json('data.name'),
+        ]);
+    }
+
+    public function testDestroy()
+    {
+        $author = Author::factory()->create();
+
+        $response = $this->deleteJson(route('api.authors.destroy', $author->id));
+
+        $response->assertNoContent();
+        assertEmpty(Author::find($author->id));
+    }
+}
---
+++ tests/Feature/Api/BookControllerTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Author;
+use App\Models\Book;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BookControllerTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+    public function testIndex()
+    {
+        $response = $this->getJson(route('api.books.index'));
+        $response->assertOk();
+    }
+
+    public function testIndexWithAuthorFilter()
+    {
+        $newBook = Book::factory()->create();
+        $response = $this->getJson(route('api.books.index', [
+            'author' => [$newBook->author_id],
+        ]));
+
+        $response->assertOk();
+        $response->assertJsonCount(1, 'data');
+    }
+
+    public function testIndexWithAuthorFilterAndInvalidAuthorId()
+    {
+        $maxAuthorId = Author::orderBy('id', 'desc')->first()?->id ?? 0;
+
+        $response = $this->getJson(route('api.books.index', [
+            'author' => [$maxAuthorId + 1],
+        ]));
+
+        $response->assertOk();
+        $response->assertJsonCount(0, 'data');
+    }
+
+    public function testStore()
+    {
+        $author = Author::factory()->create();
+        $response = $this->postJson(route('api.books.store'), [
+            'title' => $this->faker->title,
+            'author_id' => $author->id,
+        ]);
+
+        $response->assertCreated();
+        $this->assertDatabaseHas('books', [
+            'id' => $response->json('data.id'),
+            'title' => $response->json('data.title'),
+            'author_id' => $response->json('data.author_id'),
+        ]);
+    }
+
+    public function testShow()
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->getJson(route('api.books.show', $book->id));
+
+        $response->assertOk();
+    }
+
+    public function testUpdate()
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->putJson(route('api.books.update', $book->id), [
+            'title' => $this->faker->title,
+            'author_id' => Author::factory()->create()->id,
+        ]);
+
+        $response->assertOk();
+        $this->assertDatabaseHas('books', [
+            'id' => $book->id,
+            'title' => $response->json('data.title'),
+            'author_id' => $response->json('data.author_id'),
+        ]);
+    }
+
+    public function testDestroy()
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->deleteJson(route('api.books.destroy', $book->id));
+
+        $response->assertNoContent();
+        $this->assertDatabaseMissing('books', [
+            'id' => $book->id,
+        ]);
+    }
+}
