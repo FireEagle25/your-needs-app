e05586f1af4362c41f4f6ec67968c5d58d124ad2
Nechaev Pavel <nechaev.p.v.25@gmail.com>
2023-05-04 17:11:02 +0700

    chore: в базовый сидер добавлены сидеры книг и авторов

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,32 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
+    /**
+     * Seeders to run.
+     *
+     * @var array<string>
+     */
+    private array $seeders = [
+        BookSeeder::class,
+        AuthorSeeder::class
+    ];
+
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($this->seeders as $seeder) {
+            $this->call($seeder);
+        }
     }
 }
